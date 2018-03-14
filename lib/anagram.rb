37cda5e1ca294ef4684b26dcfa3014ba2f5ef614
Brandon Chin <brandonchin@me.com>
2018-03-14 23:28:51 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,21 @@
 require 'pry'
-class Anagram
-  attr_accessor :anagram
+ class Anagram
+   attr_accessor :anagram
 
-  def initialize(anagram)
-    @anagram = anagram
-  end #def initialize
+   def initialize(anagram)
+     @anagram = anagram
+   end #def initialize
 
-  def match(words)
-    answer_array = []
-    anagram_array = anagram.split("").sort
-    words.each do |one_word|
-      one_word_array = one_word.split("").sort
-      if anagram_array == one_word_array
-        answer_array << one_word
-      end #if
-    end #words.each do
-    answer_array
-  end #def match
+   def match(words)
+     answer_array = []
+     anagram_array = anagram.split("").sort
+     words.each do |one_word|
+       one_word_array = one_word.split("").sort
+       if anagram_array == one_word_array
+         answer_array << one_word
+       end #if
+     end #words.each do
+     answer_array
+   end #def match
 
-end #class Anagram
+ end #class Anagram
